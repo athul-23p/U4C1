94fdeb2bd9a445cdc6a254a99ef43b07b90c9838
athul23p <athul238p@gmail.com>
2022-03-07 12:03:20 +0530

    implemented checkPermission middleware

--- index.js
+++ index.js
@@ -7,17 +7,27 @@ app.get('/books',(req,res)=>{
     res.json({route:"/books"});
 });
 
-app.get('/libraries',(req,res)=>{
-    res.json({route:"/libraries",permission:true});
+app.get('/libraries',checkPermission('librarian'),(req,res)=>{
+    res.json({route:"/libraries",permission: req.permission});
 });
 
-app.get('/authors',(req,res)=>{
-    res.json({route:'/authors',permission:true});
+app.get('/authors',checkPermission('author'),(req,res)=>{
+    res.json({route:'/authors',permission: req.permission});
 });
 
 function logger(req,res,next){
     console.log(req.originalUrl);
     next();
+}
+
+function checkPermission(data){
+    return (req,res,next) => {
+        if(req.originalUrl === '/libraries' || req.originalUrl === '/authors'){
+            req.permission = true;
+            // console.log(data);
+        }
+        next();
+    }
 }
 app.listen(port,()=>{console.log(`listening on ${port}`)});
 
